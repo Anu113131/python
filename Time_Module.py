815bd85d5b5e20c0154f29cec5fe10af1782f17a
Anu113131 <108284556+Anu113131@users.noreply.github.com>
2022-08-08 17:51:48 +0530

    Learned Module

---
+++ Time_Module.py
@@ -0,0 +1,8 @@
+import timer
+
+print("Hello World")
+
+Name=input('Enter your Name : ')
+print("Your name will be printed After 2 seconds .")
+timer.sleep(2)
+print("Hai ",Name,"Welcome to Python")
